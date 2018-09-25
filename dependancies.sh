dc11bdf263981bfc891ba3ac488edd42a48c8be5
Guillaume Morin-Duponchelle <43368763+GuillaumeMorinDuponchelle@users.noreply.github.com>
2018-09-25 12:28:00 +0200

    Create dependancies.sh

---
+++ dependancies.sh
@@ -0,0 +1,21 @@
+printf 'Install for Kinetic ROS'
+printf 'Be carefull'
+
+sudo apt-get install git
+sudo apt-get install ros-kinetic-sound-play
+sudo apt-get install ros-kinetic-diagnostic-updater
+sudo apt-get install ros-kinetic-xacro
+sudo apt-get install ros-kinetic-openni2-launch
+sudo apt-get install ros-kinetic-depthimage-to-laserscan
+sudo apt-get install ros-kinetic-joystick-drivers
+sudo apt-get install ros-kinetic-imu-filter-madgwick
+sudo apt-get install ros-kinetic-robot-localization
+sudo apt-get install ros-kinetic-rtabmap
+sudo apt-get install ros-kinetic-rtabmap-ros
+sudo apt-get install ros-kinetic-robot-state-publisher
+sudo apt-get install ros-kinetic-gazebo-ros-control
+sudo apt-get install ros-kinetic-navigation
+sudo apt-get install ros-kinetic-move_base
+sudo apt-get install ros-kinetic-navfn
+sudo apt-get install ros-kinetic-amcl
+sudo apt-get install libusb-1.0-0-dev
